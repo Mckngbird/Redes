9e687c257608a3867465671bb0dd3e31f1e0945d
José Rivera <105292665+Mckngbird@users.noreply.github.com>
2023-07-20 20:21:22 -0400

    Redes
    Tarea 2 de Redes

---
+++ cliente.py
@@ -0,0 +1,34 @@
+import socket
+
+# Configuración del cliente
+host = '192.168.1.15'  # Dirección IP del servidor
+port = 5000  # Puerto para la conexión
+
+# Crear un socket TCP/IP
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Conectar al servidor
+client_socket.connect((host, port))
+
+# Enviar el nombre del estudiante
+student_name = input('Ingrese su nombre: ')
+client_socket.send(student_name.encode('utf-8'))
+
+# Esperar por la respuesta del servidor antes de enviar más mensajes
+response = client_socket.recv(1024).decode('utf-8')
+print(response)
+
+# Enviar mensajes hasta que se ingrese la palabra "desconectar"
+while True:
+    message = input('Ingrese un mensaje: ')
+    client_socket.send(message.encode('utf-8'))
+    if message == 'desconectar':
+        break
+
+# Cerrar la conexión con el servidor
+client_socket.close()
+print('Conexión cerrada con el servidor')
+
+
+
+
---
+++ servidor.py
@@ -0,0 +1,55 @@
+import socket
+
+# Configuración del servidor
+host = '192.168.1.15'  # Dirección IP del servidor
+port = 5000  # Puerto para la conexión
+max_connections = 5  # Número máximo de conexiones encoladas
+
+# Crear un socket TCP/IP
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Vincular el socket al host y puerto
+server_socket.bind((host, port))
+
+# Escuchar conexiones entrantes
+server_socket.listen(max_connections)
+
+print('El servidor está listo para recibir conexiones...')
+
+connected_clients = []  # Lista para almacenar los nombres de los clientes conectados
+
+while True:
+    # Aceptar una conexión entrante
+    client_socket, client_address = server_socket.accept()
+    print(f'Cliente conectado desde: {client_address}')
+
+    # Verificar si hay demasiadas conexiones activas
+    if len(connected_clients) >= max_connections:
+        message = 'El servidor está ocupado. Por favor, espera.'
+        client_socket.send(message.encode('utf-8'))
+        client_socket.close()
+        continue
+
+    # Solicitar el nombre del estudiante
+    client_socket.send(b'Gracias por conectarse al servidor')
+    student_name = client_socket.recv(1024).decode('utf-8')
+    connected_clients.append((client_socket, student_name))
+
+    print(f'Nombre del estudiante: {student_name}')
+
+    # Recibir mensajes del cliente hasta que se reciba la palabra "desconectar"
+    while True:
+        data = client_socket.recv(1024).decode('utf-8')
+        print(f'Mensaje recibido de {student_name}: {data}')
+        if data == 'desconectar':
+            break
+
+    # Eliminar al cliente de la lista de conexiones
+    connected_clients.remove((client_socket, student_name))
+
+    # Cerrar la conexión con el cliente
+    client_socket.close()
+    print('Conexión cerrada con el cliente')
+
+
+
